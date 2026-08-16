9bc616f34e8a92ca907ba09b6a034c4ac6494be7
jinafaizi <faizie.jina@gmail.com>
2026-08-16 21:39:09 +0330

    Update Model, Adding Validation

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStoreAPI.Models;
 
 public class Book
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+    
+    [Required]
+    public string Title { get; set; }
+    
+    [Required]
     public string? Author { get; set; }
+    
+    [Range(0.01, double.MaxValue)]
     public decimal? Price { get; set; }
+    
+    [Range(1, 5)]
     public int Rating { get; set; }
     
 }
